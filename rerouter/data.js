c32735ce19b96dcd30cf6777abc75bbcb1224992
Simon  Date <simondate@yahoo.co.uk>
2017-07-03 17:54:27 +0100

    JSON changes

--- rerouter/data.js
+++ rerouter/data.js
@@ -1,13 +1,26 @@
+// coursename_courseweek_
 // https://git.iddkingsonline.com/rerouter/index.html#exec_week1_part1_notesManager
 var data = {
   "exec": {
     "week1": {
       "part1": {
-        "manager": "mod/oublog/view.php?id=1776236",
-        "new": "mod/oublog/editpost.php?blog=748",
+        "notes": {
+          "manager": {
+            "1": "mod/oublog/view.php?id=1776236",
+            "2": "run2"
+          },
+          "new": {
+            "1": "mod/oublog/editpost.php?blog=748",
+            "2": "run2"
+          }
+        },
         "social": {
-          "1": "mod/forum/view.php?id=1608076",
-          "2": "reddit.com"
+          "activity1": {
+            "1": "mod/forum/view.php?id=1608076"
+          },
+          "activity2": {
+            "1": "run 1"
+          }
         }
       }
     }

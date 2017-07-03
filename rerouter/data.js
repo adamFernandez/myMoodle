d5f90ffbeafb36fa6a5125d277f8ec333bbbafb1
Simon  Date <simondate@yahoo.co.uk>
2017-07-03 13:27:23 +0100

    Updated for 1st test

--- rerouter/data.js
+++ rerouter/data.js
@@ -1,11 +1,12 @@
+// https://git.iddkingsonline.com/rerouter/index.html#exec_week1_part1_notesManager
 var data = {
   "exec": {
     "week1": {
       "part1": {
-        "notesManager": "myNotes",
-        "newNote": "newNote",
+        "manager": "mod/oublog/view.php?id=1776236",
+        "new": "mod/oublog/editpost.php?blog=748",
         "social": {
-          "1": "google.com",
+          "1": "mod/forum/view.php?id=1608076",
           "2": "reddit.com"
         }
       }

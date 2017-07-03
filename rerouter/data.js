724bd8dfcbb769d5f5688f030178bb5a084ff46d
Simon  Date <simondate@yahoo.co.uk>
2017-07-03 13:02:15 +0100

    Working version that descends through a JSON file and finds a url that it can reroute to.

---
+++ rerouter/data.js
@@ -0,0 +1,15 @@
+var data = {
+  "exec": {
+    "week1": {
+      "part1": {
+        "notesManager": "myNotes",
+        "newNote": "newNote",
+        "social": {
+          "1": "google.com",
+          "2": "reddit.com"
+        }
+      }
+    }
+  },
+  "iccl": {}
+};

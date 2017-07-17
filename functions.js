d755192ff29c6c8ccc705aa7d113d6122776592e
Simon  Date <simondate@yahoo.co.uk>
2017-07-17 17:07:26 +0100

    fix

--- functions.js
+++ functions.js
@@ -134,7 +134,7 @@ function getCurrentWeek(weeks) {
   for(var i = 0 ; i <= weeks.length ; i++){
     console.log(i);
     console.log(weeks.length);
-    if(week > e)
+    if(weeks[i] > e)
     console.log("it's bigger");
     else
     console.log("It's smaller");

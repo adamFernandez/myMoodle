7db978efd938b4113bbe4edc7e4be63184ee10a6
Louise <louise.bennett@kcl.ac.uk>
2017-08-01 15:24:08 +0100

    Update data.js

--- route/data.js
+++ route/data.js
@@ -28,22 +28,12 @@ var data = {
   "maritime": {
  	    "week1": {
       "part1": {
-        "notes": {
-          "manager": {
-            "1": "mod/oublog/view.php?id=1776236",
-            "2": "run2"
-          },
-          "new": {
-            "1": "mod/oublog/editpost.php?blog=748",
-            "2": "run2"
-          }
-        },
         "social": {
           "act1": {
-            "1": "mod/forum/view.php?id=1608076"
+            "1": "mod/forum/view.php?id=1608981"
           },
           "act2": {
-            "1": "run 1"
+            "1": "mod/forum/view.php?id=1608980"
           }
         }
       }

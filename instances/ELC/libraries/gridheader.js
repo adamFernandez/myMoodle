c5f6bcd2d2ae998a27c91f73f66e98d39192eb03
jannukas <jannukas@gmail.com>
2018-06-18 17:08:35 +0100

    Update gridheader.js

--- instances/ELC/libraries/gridheader.js
+++ instances/ELC/libraries/gridheader.js
@@ -1,1 +1,12 @@
-
+// loads in icons to use in HTML
+	loadCSS("https://git.iddkingsonline.com/instances/ELC/grid.css");
+	loadCSS("https://fonts.googleapis.com/css?family=Open+Sans");
+	function loadCSS(url){
+		var head = document.getElementsByTagName('head')[0],
+   		link = document.createElement('link');
+		link.type = 'text/css';
+   		link.rel = 'stylesheet';
+   		link.href = url;
+   		head.appendChild(link);
+   		return link;
+	}

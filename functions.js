6d0455384e03f4e7a0b9c47758f6499ed2513d25
jannukas <jannukas@gmail.com>
2018-08-06 13:01:12 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -151,3 +151,28 @@ function drawLogo(location) {
   logo.attr("src", location);
   logo.css("width", 50).css("height", 50);
 }
+
+function getSize() {
+  size = $( "p" ).css( "font-size" );
+  size = parseInt(size, 10);
+  $( "#font-size" ).text(  size  );
+}
+
+//get inital font size
+getSize();
+
+$( "#up" ).on( "click", function() {
+
+  // parse font size, if less than 50 increase font size
+  if ((size + 2) <= 50) {
+    $( "p" ).css( "font-size", "+=2" );
+    $( "#font-size" ).text(  size += 2 );
+  }
+});
+
+$( "#down" ).on( "click", function() {
+  if ((size - 2) >= 12) {
+    $( "p" ).css( "font-size", "-=2" );
+    $( "#font-size" ).text(  size -= 2  );
+  }
+});

a5d929f40aa65f94b2d4d3cf986e9f1790a3b351
jannukas <jannukas@gmail.com>
2018-11-20 16:16:16 +0000

    Add files via upload

---
+++ instances/PEPP/libraries/gridheader.js
@@ -0,0 +1,13 @@
+
+// loads in icons to use in HTML
+	loadCSS("https://git.iddkingsonline.com/instances/ELC/grid.css");
+	loadCSS("https://fonts.googleapis.com/css?family=Open+Sans");
+	function loadCSS(url){
+		var head = document.getElementsByTagName('head')[0],
+   		link = document.createElement('link');
+		link.type = 'text/css';
+   		link.rel = 'stylesheet';
+   		link.href = url;
+   		head.appendChild(link);
+   		return link;
+	}

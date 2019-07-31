96588c7742c61d2bcae283ecb6fe80dac6c0c717
paulgi <paul.gillary@kcl.ac.uk>
2019-07-31 15:58:57 +0100

    Update data.js

--- route/data.js
+++ route/data.js
@@ -627,7 +627,13 @@ var data = {
       "module6b": {
         "forum1": {
           "1": "mod/forum/view.php?id=2634741"
-         }
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=2634769"
+        },
+        "forum3": {
+          "1": "mod/forum/view.php?id=2634798"
+        }
       }
     }
 };

45a86af56655e77f8d6e13e14b178b2a33d8acdb
Ephie <ephie.lam@kcl.ac.uk>
2019-08-16 15:32:18 +0100

    Sidebar toggle menu

--- designsystem/js/main.js
+++ designsystem/js/main.js
@@ -3,6 +3,6 @@ $(document).on("click", ".view-close-transcript", function(event) {
   $(this).text($(this).text() == 'View transcript' ? 'Hide transcript' : 'View transcript');
 });
 
-$(document).on("click", "#blcok-region-side-post.block:not(.block_fake) .title h2", function(event) {
+$(document).on("click", "#block-region-side-post.block:not(.block_fake) .title h2", function(event) {
   $(this).parents().block.toggleClass('hidden');
 });

f550fa2eefe0b8c5156949f07c44f2c8ed762e13
Ephie <ephie.lam@kcl.ac.uk>
2019-08-19 09:32:36 +0100

    toggle side bar menus

--- designsystem/js/main.js
+++ designsystem/js/main.js
@@ -3,6 +3,7 @@ $(document).on("click", ".view-close-transcript", function(event) {
   $(this).text($(this).text() == 'View transcript' ? 'Hide transcript' : 'View transcript');
 });
 
+// toggle side bar menus
 $(document).on("click", "#block-region-side-post .block .title h2", function(event) {
   $(this).parents(".block").toggleClass('hidden');
 });

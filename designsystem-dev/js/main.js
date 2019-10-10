0fab9292d2a101e8a66081a90dcf95c501ef48c7
Ephie <ephie.lam@kcl.ac.uk>
2019-10-10 16:30:13 +0100

    Book nav: show one more page for first and last

--- designsystem-dev/js/main.js
+++ designsystem-dev/js/main.js
@@ -61,8 +61,14 @@ $(".navbottom.clearfix.navtext ul li a, .navbottom.clearfix.navtext ul li strong
 });
 // add current class to current page
 $(".navbottom.clearfix.navtext ul li :not(a)").parents("li").addClass("current");
-// add prev and next class to li before and after current
+// add prev and next class to li before and after current for mobile
 $(".chapter.current").prev("li").addClass("prev");
 $(".chapter.current").next("li").addClass("next");
+// show one more page if first or last page on mobile
+if ($(".navbottom a.bookprev").length == 0) {
+  $("li.chapter:nth-child(3)").addClass("next");
+} else if ($(".navbottom a.booknext").length == 0) {
+  $("li.chapter:nth-last-child(3)").addClass("prev");
+}
 // remove text from previous and next buttons
 $(".navbottom.clearfix.navtext > a").empty();

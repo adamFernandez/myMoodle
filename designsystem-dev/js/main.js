6dd510e4028fd4a052bb13070c04bc883975702a
Ephie <ephie.lam@kcl.ac.uk>
2020-01-08 09:53:39 +0000

    Administration block admin view

--- designsystem-dev/js/main.js
+++ designsystem-dev/js/main.js
@@ -1,6 +1,6 @@
 // hide administration block from students or teachers without editing rights
 //$("#block-region-side-post .block:has(.header .title h2:contains('Administration')):not(:has(.content #settingsnav ul li ul li a:contains('Edit settings')))").hide();
-$("#block-region-side-post > div.block:last-of-type").hide();
+$("#block-region-side-post > div.block:first-of-type").hide();
 
 // toggle side bar menus
 const blockHide = "#block-region-side-pre .block .title h2, #block-region-side-post .block .title h2";

3dd55c5550c3f94f0191f9def769e400404cee1e
Ephie <ephie.lam@kcl.ac.uk>
2020-01-16 11:32:40 +0000

    Activity labels do not clone to quickeditlink

--- designsystem-dev/js/activity-labels.js
+++ designsystem-dev/js/activity-labels.js
@@ -1,5 +1,5 @@
 // label code
-$("li.activity .activityinstance a").append('<div class="activity-label"><div class="assessed-label">Assessed</div><div class="primary-label"><i></i><span class="label-text"></span></div><div class="media-icon"><i></i></div></div>');
+$("li.activity .activityinstance a:not(.quickeditlink)").append('<div class="activity-label"><div class="assessed-label">Assessed</div><div class="primary-label"><i></i><span class="label-text"></span></div><div class="media-icon"><i></i></div></div>');
 
 // study type no icon
 // 'file' activity type has 'resource' class

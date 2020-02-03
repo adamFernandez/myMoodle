10ea1a8f1c59407fabc0c017c7aa4cd7d2adcb2b
Ephie <ephie.lam@kcl.ac.uk>
2020-02-03 16:06:10 +0000

    Activity labels: model 5 single label option to downgrade labels

--- designsystem-dev/js/activity-labels.js
+++ designsystem-dev/js/activity-labels.js
@@ -7,23 +7,28 @@ $("li.activity .instancename .accesshide").each(function() {
     $(this).parents(".activityinstance").prepend(this);
 });
 
+// set defaults
 // study type no icon
 // 'file' activity type has 'resource' class
 $("li.activity.book, li.activity.folder, li.activity.page, li.activity.resource, li.activity.glossary, li.activity.lesson, li.activity.lti").addClass("type-study");
 // study type with icon
-$("li.activity.kalvidres, li.activity.url").addClass("type-study play");
+$("li.activity.kalvidres, li.activity.url").addClass("type-study i-media");
 // activity type no icon
 $("li.activity.assign, li.activity.choice, li.activity.feedback, li.activity.hvp, li.activity.kalvidassign, li.activity.oublog, li.activity.questionnaire, li.activity.quiz, li.activity.turnitintooltwo, li.activity.workshop").addClass("type-activity");
 // activity type with icon
-$("li.activity.data, li.activity.forum, li.activity.connecthosted, li.activity.wiki").addClass("type-activity group");
+$("li.activity.data, li.activity.forum, li.activity.connecthosted, li.activity.wiki").addClass("type-activity i-group");
 
 // add assessed label
-$("li.activity .instancename:contains('-ass')").parents("li.activity").addClass("type-assessed");
+$("li.activity .instancename:contains('-ass')").parents("li.activity").removeClass("type-study type-activity").addClass("type-assessed");
 // override activity type
-$("li.activity .instancename:contains('-act')").parents("li.activity").removeClass("type-study").addClass("type-activity");
-// add icon
+$("li.activity .instancename:contains('-act')").parents("li.activity").removeClass("type-study type-assessed").addClass("type-activity");
+// remove activity label
+$("li.activity .instancename:contains('-stu')").parents("li.activity").removeClass("type-activity type-assessed").addClass("type-study");
+// add or remove icon
 $("li.activity .instancename:contains('-gro')").parents("li.activity").addClass("i-group");
+$("li.activity .instancename:contains('-ngr')").parents("li.activity").removeClass("i-group");
 $("li.activity .instancename:contains('-med')").parents("li.activity").addClass("i-media");
+$("li.activity .instancename:contains('-nme')").parents("li.activity").removeClass("i-media");
 
 // strip keywords from activity title
 $("li.activity .instancename:contains('activity-label'), #region-main h2:first-of-type:contains('activity-label'), .breadcrumb li a span:contains('activity-label'), .breadcrumb li a:contains('activity-label'), .row-fluid.rtl-compatible .span4 a:contains('activity-label')").text(function(i, currentText) {

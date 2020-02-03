9b629837d75558553ffe78b02a3ea339492f9152
Ephie <ephie.lam@kcl.ac.uk>
2020-02-03 14:17:01 +0000

    Activity labels: model 5 single label

--- designsystem-dev/js/activity-labels.js
+++ designsystem-dev/js/activity-labels.js
@@ -1,5 +1,5 @@
 // label code
-$("li.activity .activityinstance a:not(.quickeditlink)").append('<div class="activity-label"><div class="assessed-label">Assessed</div><div class="primary-label"><i></i><span class="label-text"></span></div><div class="media-icon"><i></i></div></div>');
+$("li.activity .activityinstance a:not(.quickeditlink)").append('<div class="activity-label-container"><div class="primary-label"><i></i><span class="label-text"></span></div><div class="group-icon"><i></i></div><div class="media-icon"><i></i></div></div>');
 
 // move .accesshide from within .instancename and append to .activityinstance
 // affected the prefix title modification in original location
@@ -15,19 +15,19 @@ $("li.activity.kalvidres, li.activity.url").addClass("type-study play");
 // activity type no icon
 $("li.activity.assign, li.activity.choice, li.activity.feedback, li.activity.hvp, li.activity.kalvidassign, li.activity.oublog, li.activity.questionnaire, li.activity.quiz, li.activity.turnitintooltwo, li.activity.workshop").addClass("type-activity");
 // activity type with icon
-$("li.activity.data, li.activity.forum, li.activity.connecthosted, li.activity.wiki").addClass("type-activity icon");
+$("li.activity.data, li.activity.forum, li.activity.connecthosted, li.activity.wiki").addClass("type-activity group");
 
 // add assessed label
 $("li.activity .instancename:contains('-ass')").parents("li.activity").addClass("type-assessed");
 // override activity type
 $("li.activity .instancename:contains('-act')").parents("li.activity").removeClass("type-study").addClass("type-activity");
 // add icon
-$("li.activity .instancename:contains('-ico')").parents("li.activity").addClass("icon");
+$("li.activity .instancename:contains('-ico')").parents("li.activity").addClass("group");
 $("li.activity .instancename:contains('-med')").parents("li.activity").addClass("play");
 
 // strip keywords from activity title
 $("li.activity .instancename:contains('activity-label'), #region-main h2:first-of-type:contains('activity-label'), .breadcrumb li a span:contains('activity-label'), .breadcrumb li a:contains('activity-label'), .row-fluid.rtl-compatible .span4 a:contains('activity-label')").text(function(i, currentText) {
-  return currentText.substring(31);
+  return currentText.substring(27);
 })
 
 // add indent class and remove keyword
@@ -38,11 +38,11 @@ $("#region-main h2:first-of-type:contains('-indent'), .breadcrumb li a span:cont
   return currentText.substring(8);
 })
 
-// add study text to study type label
-$("li.activity.type-study .activityinstance a .activity-label .primary-label .label-text").text("study");
+// add assessed text to assessed type label
+$("li.activity.type-assessed .activityinstance a .activity-label-container .activity-label .label-text").text("assessed");
 // add activity text to activity type label
-$("li.activity.type-activity .activityinstance a .activity-label .primary-label .label-text").text("activity");
+$("li.activity.type-activity .activityinstance a .activity-label-container .activity-label .label-text").text("activity");
 // add group icon to activity type label
-$("li.activity.type-activity.icon .activityinstance a .activity-label .primary-label i").addClass("fas fa-user-friends");
+$("li.activity.group .activityinstance a .activity-label-container .group-icon i").addClass("fas fa-user-friends");
 // add media icon
-$("li.activity.play .activityinstance a .activity-label .media-icon i").addClass("fas fa-play-circle");
+$("li.activity.play .activityinstance a .activity-label-container .media-icon i").addClass("fas fa-play-circle");

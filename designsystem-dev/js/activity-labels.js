5993f65ab36e61a9b38b2c8bb36a0989270f7c70
Ephie <ephie.lam@kcl.ac.uk>
2020-02-10 13:11:05 +0000

    Activity labels: remove prefix from document title

--- designsystem-dev/js/activity-labels.js
+++ designsystem-dev/js/activity-labels.js
@@ -1,7 +1,3 @@
-var title = document.title;
-if (title.includes("activity-label")) {
-  $(document).attr("title", title.substr(20));
-}
 // label code
 $("li.activity .activityinstance a:not(.quickeditlink)").append('<div class="activity-label-container"><div class="activity-label"><i></i><span class="label-text"></span></div><div class="group-icon"><i></i></div><div class="media-icon"><i></i></div></div>');
 
@@ -38,6 +34,7 @@ $("li.activity .instancename:contains('-nme')").parents("li.activity").removeCla
 $("li.activity .instancename:contains('activity-label'), #region-main h2:first-of-type:contains('activity-label'), .breadcrumb li a span:contains('activity-label'), .breadcrumb li a:contains('activity-label'), .row-fluid.rtl-compatible .span4 a:contains('activity-label')").text(function(i, currentText) {
   return currentText.substr(27);
 })
+// and document title
 var documentTitle = document.title;
 if (documentTitle.includes('activity-label')) {
   $(document).attr('title', documentTitle.substr(27));

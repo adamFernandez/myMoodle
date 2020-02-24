5fdadb1c252e2145f382faa556f7f6330bf0ca4d
Ephie <ephie.lam@kcl.ac.uk>
2020-02-24 14:52:34 +0000

    Print: remove activity label prefix from book info table

--- designsystem-dev/js/main.js
+++ designsystem-dev/js/main.js
@@ -130,12 +130,7 @@ $("li.activity .instancename:contains('-med')").parents("li.activity").addClass(
 $("li.activity .instancename:contains('-nme')").parents("li.activity").removeClass("i-media");
 
 // strip keywords from activity title
-$("li.activity .instancename:contains('activity-label'), #region-main h2:first-of-type:contains('activity-label'),
-#page-mod-book-print #page-content h1:first-of-type:contains('activity-label'),
-#page-mod-book-print #page-content .book_info td:contains('activity-label'),
-.breadcrumb li a span:contains('activity-label'), .breadcrumb li a:contains('activity-label'),
-.row-fluid.rtl-compatible .span4 a:contains('activity-label'),
-.chosted-info .chosted-info-value p:contains('activity-label')").text(function(i, currentText) {
+$("li.activity .instancename:contains('activity-label'), #region-main h2:first-of-type:contains('activity-label'), #page-mod-book-print #page-content h1:first-of-type:contains('activity-label'), #page-mod-book-print #page-content .book_info td:contains('activity-label'), .breadcrumb li a span:contains('activity-label'), .breadcrumb li a:contains('activity-label'), .row-fluid.rtl-compatible .span4 a:contains('activity-label'), .chosted-info .chosted-info-value p:contains('activity-label')").text(function(i, currentText) {
   return currentText.replace(/activity-label-[a-z]{3}-[a-z]{3}-[a-z]{3} /g, '');
 })
 if (window.matchMedia('print').matches) {

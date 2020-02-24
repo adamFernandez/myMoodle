7c546541280c517fcf7290f35efaa13cc2728d8d
Ephie <ephie.lam@kcl.ac.uk>
2020-02-24 18:03:22 +0000

    Print: correct typo

---
+++ designsystem/js/book.js
@@ -0,0 +1,15 @@
+// strip activity label keywords from activity title
+$("#page-mod-book-print #page-content h1:first-of-type:contains('activity-label'), #page-mod-book-print #page-content .book_info td:contains('activity-label')").text(function(i, currentText) {
+  return currentText.replace(/activity-label-[a-z]{3}-[a-z]{3}-[a-z]{3} /g, '');
+})
+if (window.matchMedia('print').matches) {
+  $("#page-content h1:first-of-type:contains('activity-label'), #page-content .book_info td:contains('activity-label')").text(function(i, currentText) {
+    return currentText.replace(/activity-label-[a-z]{3}-[a-z]{3}-[a-z]{3} /g, '');
+  })
+}
+// and document title
+var documentTitle = document.title;
+if (documentTitle.includes('activity-label')) {
+  documentTitle = documentTitle.replace(/activity-label-[a-z]{3}-[a-z]{3}-[a-z]{3} /g, '');
+  $(document).attr('title', documentTitle);
+}

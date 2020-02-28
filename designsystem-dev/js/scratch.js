dddd5edd50cc890518783027bdfae826a845acba
Ephie <ephie.lam@kcl.ac.uk>
2020-02-28 13:34:45 +0000

    Hide toc for single page book

--- designsystem-dev/js/scratch.js
+++ designsystem-dev/js/scratch.js
@@ -1,2 +1,2 @@
 // hide toc for single page book
-$(".block_book_toc .content ul > li:only-child").parents(".block_book_toc").hide();
+$(".block_book_toc .content ul > li:only-child strong:only-child").parents(".block_book_toc").hide();

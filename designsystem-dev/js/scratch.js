8c150c7200e85f95b70764823d93371e63a51673
Ephie <ephie.lam@kcl.ac.uk>
2020-03-02 18:06:41 +0000

    Single chapter book: print

--- designsystem-dev/js/scratch.js
+++ designsystem-dev/js/scratch.js
@@ -1,2 +1,6 @@
-// hide toc for single page book
+// hide toc for single chapter book
 $(".block_book_toc .content ul > li:only-child strong:only-child").parents(".block_book_toc").hide();
+
+// hide toc for single chapter book in print
+$("#page-mod-book-print .book_toc_numbered ul
+li:only-child").parents("#page-mod-book-print").addClass("single-chapter-book");

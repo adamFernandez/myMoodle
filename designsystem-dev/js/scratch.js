cd322ca875aa47b5c3e986e09abc1d0c0341b799
Ephie <ephie.lam@kcl.ac.uk>
2020-03-03 09:53:52 +0000

    Single chapter book: print

--- designsystem-dev/js/scratch.js
+++ designsystem-dev/js/scratch.js
@@ -3,4 +3,4 @@ $(".block_book_toc .content ul > li:only-child strong:only-child").parents(".blo
 
 // hide toc for single chapter book in print
 $("#page-mod-book-print .book_toc_numbered ul
-li:only-child").parents("#page-mod-book-print").addClass("single-chapter-book");
+li:only-child").parents("#page-content").addClass("single-chapter-book");

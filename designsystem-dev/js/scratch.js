f38142d94ed491d48c683351ca91ed3119099125
Ephie <ephie.lam@kcl.ac.uk>
2020-03-12 14:44:15 +0000

    Activity label number and title spacing test

--- designsystem-dev/js/scratch.js
+++ designsystem-dev/js/scratch.js
@@ -1,8 +1,6 @@
-// hide toc for single chapter book
-$(".block_book_toc .content ul > li:only-child strong:only-child").parents(".block_book_toc").hide();
-
-// add single-chapter-book class to screen and print to hide toc and title
-$(".block_book_toc .content ul > li:only-child, #page-mod-book-print .book_toc_numbered ul li:only-child").parents("#page-content").addClass("single-chapter-book");
-
-// hide activity labels within a specific course section
-$(".summary span.section-hide-activity-labels").parents("li.section.main").addClass("section-hide-activity-labels");
+// create activity title span to control spacing between activity number and title text
+$("li.activity span.instancename").each(function() {
+  if ($(this).text().match(/[1-9]{1}.[0-9]+\s{1}/g)) $(this).html(function(i, currentText) {
+    return currentText.replace(/ |&nbsp;/, "</span><span class='activity-title'>");
+  });
+});

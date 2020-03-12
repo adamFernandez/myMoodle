d9812128e5e8bcb1b734102d3007646ddf4c617a
Ephie <ephie.lam@kcl.ac.uk>
2020-03-12 10:03:59 +0000

    Hide activity labels in specific section

--- designsystem-dev/js/scratch.js
+++ designsystem-dev/js/scratch.js
@@ -3,3 +3,6 @@ $(".block_book_toc .content ul > li:only-child strong:only-child").parents(".blo
 
 // add single-chapter-book class to screen and print to hide toc and title
 $(".block_book_toc .content ul > li:only-child, #page-mod-book-print .book_toc_numbered ul li:only-child").parents("#page-content").addClass("single-chapter-book");
+
+// hide activity labels within a specific course section
+$(".summary span.section-hide-activity-labels").parents("li.section.main").addClass("section-hide-activity-labels");

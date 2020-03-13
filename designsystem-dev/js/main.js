39dac6561104c0fd2702655138761e650e50a159
Ephie <ephie.lam@kcl.ac.uk>
2020-03-13 11:28:11 +0000

    Activity labels: spacing between number and title

--- designsystem-dev/js/main.js
+++ designsystem-dev/js/main.js
@@ -172,3 +172,13 @@ $("li.activity.type-activity .activityinstance a .activity-label-container .acti
 $("li.activity.i-group .activityinstance a .activity-label-container .group-icon i").addClass("fas fa-user-friends");
 // add media icon
 $("li.activity.i-media .activityinstance a .activity-label-container .media-icon i").addClass("fas fa-play-circle");
+
+// create activity title span to control spacing between activity number and title text
+$("li.activity span.instancename").each(function() {
+  if ($(this).text().match(/[1-9]{1}.[0-9]{1}\s{1}/g)) $(this).html(function(i, currentText) {
+    return currentText.replace(/ |&nbsp;/, "</span><span class='activity-title'>");
+  });
+  if ($(this).text().match(/[1-9]{1}.[0-9]{2}\s{1}/g)) $(this).html(function(i, currentText) {
+    return currentText.replace(/ |&nbsp;/, "</span><span class='activity-title double-digits'>");
+  });
+});
--- designsystem-dev/js/scratch.js
+++
@@ -1,9 +0,0 @@
-// create activity title span to control spacing between activity number and title text
-$("li.activity span.instancename").each(function() {
-  if ($(this).text().match(/[1-9]{1}.[0-9]{1}\s{1}/g)) $(this).html(function(i, currentText) {
-    return currentText.replace(/ |&nbsp;/, "</span><span class='activity-title'>");
-  });
-  if ($(this).text().match(/[1-9]{1}.[0-9]{2}\s{1}/g)) $(this).html(function(i, currentText) {
-    return currentText.replace(/ |&nbsp;/, "</span><span class='activity-title double-digits'>");
-  });
-});

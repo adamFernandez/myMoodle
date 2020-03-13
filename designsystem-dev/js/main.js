e93cc0b5930159507e766fcd09047d16961246b2
Ephie <ephie.lam@kcl.ac.uk>
2020-03-13 13:58:11 +0000

    Activity title spacing same regardless of digits

--- designsystem-dev/js/main.js
+++ designsystem-dev/js/main.js
@@ -175,10 +175,7 @@ $("li.activity.i-media .activityinstance a .activity-label-container .media-icon
 
 // create activity title span to control spacing between activity number and title text
 $("li.activity span.instancename").each(function() {
-  if ($(this).text().match(/[1-9]{1}.[0-9]{1}\s{1}/g)) $(this).html(function(i, currentText) {
+  if ($(this).text().match(/[1-9]{1}.[0-9]+\s{1}/g)) $(this).html(function(i, currentText) {
     return currentText.replace(/ |&nbsp;/, "</span><span class='activity-title'>");
-  });
-  if ($(this).text().match(/[1-9]{1}.[0-9]{2}\s{1}/g)) $(this).html(function(i, currentText) {
-    return currentText.replace(/ |&nbsp;/, "</span><span class='activity-title double-digits'>");
   });
 });

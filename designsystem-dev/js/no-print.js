6b81484417af59f37fd149f976b0ad50f94cf2b8
Ephie <ephie.lam@kcl.ac.uk>
2020-04-30 09:39:31 +0100

    Activity title spacing js

--- designsystem-dev/js/no-print.js
+++ designsystem-dev/js/no-print.js
@@ -160,7 +160,7 @@ if (documentTitle.includes('activity-label')) {
 
 // create activity title span to control spacing between activity number and title text
 $("li.activity span.instancename").each(function() {
-  if ($(this).text().match(/[1-9]{1}.[0-9]+\s{1}/g)) $(this).html(function(i, currentText) {
+  if ($(this).text().match(/[1-9]{1}\.[0-9]+\s{1}/g)) $(this).html(function(i, currentText) {
     return currentText.replace(/ |&nbsp;/, "</span><span class='activity-title'>");
   });
 });

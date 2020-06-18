44363219861f25cb0d7bd97ef8ebe469d261cd23
Ephie <ephie.lam@kcl.ac.uk>
2020-06-18 17:45:29 +0100

    Test loading popper js

--- designsystem-dev/js/main.js
+++ designsystem-dev/js/main.js
@@ -1,4 +1,4 @@
-loadjs("https://code.jquery.com/jquery-3.3.1.min.js");
+loadjs("https://code.jquery.com/jquery-3.3.1.slim.min.js")
 loadjs("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js");
 loadjs("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js");
 

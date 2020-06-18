5c451a70c239518647e66d017739cbeef9e6c3dd
Ephie <ephie.lam@kcl.ac.uk>
2020-06-18 16:28:34 +0100

    Test loading popper js

--- designsystem-dev/js/main.js
+++ designsystem-dev/js/main.js
@@ -1,3 +1,4 @@
+loadjs("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js");
 loadjs("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js");
 // load external JS file into Keats
 function loadjs(url) {

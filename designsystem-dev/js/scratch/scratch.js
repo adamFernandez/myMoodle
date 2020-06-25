de7e31035d1d3bee489ba934e3885d07d14064c8
Ephie <ephie.lam@kcl.ac.uk>
2020-06-25 12:42:23 +0100

    Sidebar: hide custom code block

---
+++ designsystem-dev/js/scratch/scratch.js
@@ -0,0 +1,3 @@
+// hide custom code block when not in editing mode
+$("body:not(.editing) #block-region-side-pre .block:has(.header .title h2:contains('Custom code')),
+body:not(.editing) #block-region-side-post .block:has(.header .title h2:contains('Custom code'))").addClass("hide");

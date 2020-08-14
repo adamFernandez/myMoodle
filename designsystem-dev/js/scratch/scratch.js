92a8043b114f4f9903e12b3a5175030b7d591631
Ephie <ephie.lam@kcl.ac.uk>
2020-08-14 14:26:11 +0100

    Print transcript test

--- designsystem-dev/js/scratch/scratch.js
+++ designsystem-dev/js/scratch/scratch.js
@@ -3,50 +3,25 @@ $('body:not(.editing) #block-region-side-pre .block:has(.header .title h2:contai
 
 $(".download-transcript").click(function() {
   console.log("foo");
-  var printTitle = document.title;
   var printContent = $(this).parents('.transcript-container').children('.transcript-card').html();
   var printFeatures = 'height=600, width=800';
   var printWindow = window.open('', 'Print_Transcript', printFeatures);
   var is_safari = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
   var is_chrome = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') != -1;
 
-  if (!is_chrome) {
-    printWindow.document.write('<html><head><title>' + document.title  + '</title>');
-    if (!is_safari) {
-      console.log('not safari');
-      printWindow.document.write('<link type="text/css" rel="stylesheet" href="https://git.iddkingsonline.com/designsystem-dev/css/scratch/scratch.css">');
-    } else {
-      console.log('is safari');
-    }
-    printWindow.document.write('</head><body><h1>' + document.title  + '</h1>');
-    printWindow.document.write(printContent);
-    printWindow.document.write('</body></html>');
+  printWindow.document.write('<html><head><title>' + document.title  + '</title>');
+  if (!is_safari) {
+    console.log('not safari');
+    printWindow.document.write('<link type="text/css" rel="stylesheet" href="https://git.iddkingsonline.com/designsystem-dev/css/scratch/scratch.css">');
   } else {
-    console.log('is chrome');
-    printWindow.document.write('<html><head><title>' + document.title  + '</title><link type="text/css" rel="stylesheet" href="https://git.iddkingsonline.com/designsystem-dev/css/scratch/scratch.css"></head><body><h1>' + document.title  + '</h1>');
-    printWindow.document.write(printContent);
-    printWindow.document.write('</body></html>');
+    console.log('is safari');
   }
-  printWindow.document.close(); // necessary for IE >= 10
-  printWindow.focus(); // necessary for IE >= 10
-  printWindow.print();
-  printWindow.close();
-  return true;
-/*  
-  var printContent = $(this).parents('.transcript-container').children('.transcript-card').html();
-  var printWindow = window.open('', 'PRINT', 'height=600, width=800');
-  
-  printWindow.document.write(
-    '<html><head><title>' + document.title  + '</title><link type="text/css" rel="stylesheet" href="https://git.iddkingsonline.com/designsystem-dev/css/main.css"></head><body><h1>' + document.title  + '</h1>');
+  printWindow.document.write('</head><body><h1>' + document.title  + '</h1>');
   printWindow.document.write(printContent);
   printWindow.document.write('</body></html>');
-
   printWindow.document.close(); // necessary for IE >= 10
   printWindow.focus(); // necessary for IE >= 10
-
   printWindow.print();
   printWindow.close();
-
   return true;
-  */
 })

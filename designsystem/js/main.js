05afa2aa2cdc9e87b31e258ff87fdf38dcb5c511
Ephie <ephie.lam@kcl.ac.uk>
2020-09-14 12:29:43 +0100

    Activity number spacing to accommodate doubgle digits

--- designsystem/js/main.js
+++ designsystem/js/main.js
@@ -256,7 +256,7 @@ if (documentTitle.includes('activity-label')) {
 
 // create activity title span to control spacing between activity number and title text
 $("li.activity span.instancename").each(function() {
-  if ($(this).text().match(/^[1-9]+\.[0-9]+\s{1}/g)) $(this).html(function(i, currentText) {
+  if ($(this).text().match(/^[1-9]+[0-9]?\.[0-9]+\s{1}/g)) $(this).html(function(i, currentText) {
     $(this).addClass('is-numbered');
     return currentText.replace(/ |&nbsp;/, "</span><span class='activity-title'>");
   });

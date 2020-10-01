63acf3a559ecb888cdbde44cf9a8e3627adc8a32
sarahjanegrizi <57497830+sarahjanegrizi@users.noreply.github.com>
2020-10-01 22:21:36 +0100

    Update data.js

--- route/data.js
+++ route/data.js
@@ -368,22 +368,22 @@ var data = {
     "module1": {
       "general": {
         "1": "mod/forum/view.php?id=2951321",
-        "2": "mod/forum/view.php?id=3439085"
+        "2": "mod/forum/view.php?id=4519447"
       },
       "week1": {
         "part3": {
           "social": {
             "act1": {
               "1": "mod/choice/view.php?id=2951338",
-              "2": "mod/choice/view.php?id=3439106"
+              "2": "mod/choice/view.php?id=4519470"
             }
           }
         }
       },
       "week3": {
         "activity": {
-          "1": "mod/forum/discuss.php?d=232574",
-          "2": "mod/forum/discuss.php?d=232574"
+          "1": "mod/forum/discuss.php?d=4519447",
+          "2": "mod/forum/discuss.php?d=4519447"
         }
       }
     },
@@ -396,7 +396,7 @@ var data = {
     "module3": {
       "discussion": {
         "1": "mod/forum/view.php?id=3096103",
-        "2": "mod/forum/view.php?id=3222987"
+        "2": "mod/forum/view.php?id=4519681"
       }
     },
     "module4": {
@@ -454,12 +454,12 @@ var data = {
     },
     "module7": {
       "discussion": {
-        "1": "mod/forum/view.php?id=3182936"
+        "1": "mod/forum/view.php?id=4519901"
       }
     },
      "module8": {
       "discussion": {
-        "1": "mod/forum/view.php?id=3183098"
+        "1": "mod/forum/view.php?id=4520146"
       },
       "w1": {
         "forum1": {
@@ -474,26 +474,26 @@ var data = {
     },
     "module10": {
       "discussion": {
-        "1": "mod/forum/view.php?id=3439246",
+        "1": "mod/forum/view.php?id=4520673",
         "2": "mod/forum/view.php?id=2942155"
       }
     },
     "module11": {
       "discussion": {
         "1": "mod/forum/view.php?id=2958006",
-        "2": "mod/forum/view.php?id=3439959"
+        "2": "mod/forum/view.php?id=4520813"
       }
     },
     "module12": {
       "discussion": {
-        "1": "mod/forum/view.php?id=3440015",
+        "1": "mod/forum/view.php?id=4520886",
         "2": "mod/forum/view.php?id=2960026"
       }
     },
     "module13": {
       "discussion": {
         "1": "mod/forum/view.php?id=2948091",
-        "2": "mod/forum/view.php?id=3440086"
+        "2": "mod/forum/view.php?id=4520986"
       }
     }
   },

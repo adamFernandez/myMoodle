5d466535c6d0c11e00c1dcacae8ebd4577355e7d
Alesito <aarodriguezfernandez@gmail.com>
2021-06-08 16:23:19 +0100

    Another commit

---
+++ route/data.js
@@ -0,0 +1,753 @@
+
+// coursename_courseweek_run
+// If you make a large update you can test you didn't break it using the link below. If it doesn't re-route you may have broken it!
+// https://git.iddkingsonline.com/route/index.html#exec_week1_part1_notes_manager_1
+var data = {
+  "simonSandbox": {
+    "comments": {
+      "1": "mod/forum/view.php?id=1573957"
+    }
+  },
+  "usertesting": {
+    "comments": {
+      "1": "mod/forum/view.php?id=2299107"
+    }
+  },
+  "exec": {
+    "week1": {
+      "part1": {
+        "notes": {
+          "manager": {
+            "1": "mod/oublog/view.php?id=1776236",
+            "2": "run2"
+          },
+          "new": {
+            "1": "mod/oublog/editpost.php?blog=748",
+            "2": "run2"
+          }
+        },
+        "social": {
+          "act1": {
+            "1": "mod/forum/view.php?id=1608076"
+          },
+          "act2": {
+            "1": "run 1"
+          }
+        }
+      }
+    },
+    "pubman": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=2128534"
+      },
+      "discussion2": {
+        "1": "mod/forum/view.php?id=2128565"
+      }
+    }
+  },
+  "maritime": {
+    "module1": {
+      "week3": {
+        "discussion": {
+          "1": "mod/forum/view.php?id=2398355",
+          "2": "mod/forum/view.php?id=2510395",
+          "3": "mod/forum/view.php?id=2398532",
+        }
+      },
+      "week5": {
+        "discussion": {
+          "1": "mod/forum/view.php?id=2398401",
+          "2": "mod/forum/view.php?id=2510409",
+          "3": "mod/forum/view.php?id=2398579",
+        }
+      }
+    },
+    "module3": {
+      "week1": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=2605955"
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=2605946"
+        },
+        "forum3": {
+          "1": "mod/forum/view.php?id=2605966"
+        }
+      },
+      "week2": {
+        "1": "mod/forum/view.php?id=2605986"
+      },
+      "week3": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=2606019"
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=2606030"
+        }
+      }
+    },
+    "module4": {
+      "forum1": {
+        "1": "mod/forum/view.php?id=4858222"
+      },
+      "forum2": {
+        "1": "mod/forum/view.php?id=4858248"
+      },
+      "forum3": {
+        "1": "mod/forum/view.php?id=4858280"
+      }
+    }
+  },
+  "iccl": {
+    "module1": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4830264"
+      },
+      "quiz1": {
+        "1": "mod/quiz/view.php?id=4830294"
+      },
+      "quiz2": {
+        "1": "mod/quiz/view.php?id=4830297"
+      },
+      "quiz3": {
+        "1": "mod/quiz/view.php?id=4830328"
+      },
+      "quiz4": {
+        "1": "mod/quiz/view.php?id=4830345"
+      },
+      "quiz5": {
+        "1": "mod/quiz/view.php?id=4830352"
+      },
+      "quiz6": {
+        "1": "mod/quiz/view.php?id=4830389"
+      }
+    },
+    "module2": {
+      "comments": {
+        "1": "mod/forum/view.php?id=3224640"
+      }
+    },
+    "module3": {
+      "comments": {
+        "1": "mod/forum/view.php?id=3299988"
+      }
+    },
+    "module4": {
+      "comments": {
+        "1": "mod/forum/view.php?id=3445949"
+      }
+    },
+    "module5": {
+      "comments": {
+        "1": "mod/forum/view.php?id=3445806"
+      }
+    },
+    "module6": {
+      "comments": {
+        "1": "mod/forum/view.php?id=3445689"
+      }
+    },
+    "module7": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4830668"
+      },
+      "poll1": {
+        "1": "mod/choice/view.php?id=4830696"
+      },
+      "poll2": {
+        "1": "mod/choice/view.php?id=4830738"
+      },
+      "poll3": {
+        "1": "mod/choice/view.php?id=4830783"
+      },
+      "poll4": {
+        "1": "mod/choice/view.php?id=4830784"
+      }
+    },
+    "module8": {
+      "comments": {
+        "1": "mod/forum/view.php?id=2956978"
+      },
+      "poll1": {
+        "1": "mod/choice/view.php?id=2957004"
+      }
+    },
+    "module9": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4365445"
+      },
+      "quiz1": {
+        "1": "mod/quiz/view.php?id=4365473"
+      },
+      "quiz2": {
+        "1": "mod/quiz/view.php?id=4365508"
+      },
+      "forum1": {
+        "1": "mod/forum/view.php?id=4365453"
+      },
+      "poll1": {
+        "1": "mod/choice/view.php?id=3300331"
+      }
+    },
+    "module10": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4405313",
+        "2": "mod/forum/view.php?id=4405313"
+      }
+    },
+    "module11": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4572098"
+      }
+    },
+    "module12": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4734468"
+      }
+    }
+  },
+  "ifcl": {
+    "module13": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4365572"
+      }
+    },
+    "module15": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4921021"
+      },
+      "comments2": {
+        "1": "mod/forum/view.php?id=4921021"
+      },
+      "comments3": {
+        "1": "mod/forum/view.php?id=2957487"
+      },
+      "comments4": {
+        "1": "mod/forum/view.php?id=2957483"
+      }
+    },
+    "module16": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4830082"
+      }
+    },
+    "module5": {
+      "forum1": {
+        "1": "mod/forum/view.php?id=4405470"
+      },
+      "comments": {
+        "1": "mod/forum/view.php?id=4405445"
+      },
+      "poll1": {
+        "1": "mod/choice/view.php?id=4405489"
+      },
+      "forum2": {
+        "1": "mod/forum/view.php?id=2538368"
+      },
+      "forum3": {
+        "1": "mod/forum/view.php?id=4405508"
+      },
+      "forum4": {
+        "1": "mod/forum/view.php?id=4405503"
+      },
+      "forum5": {
+        "1": "mod/forum/view.php?id=4405504"
+      },
+      "forum6": {
+        "1": "mod/forum/view.php?id=4405511"
+      },
+      "forum7": {
+        "1": "mod/forum/view.php?id=4405538"
+      },
+      "forum8": {
+        "1": "mod/forum/view.php?id=4405541"
+      },
+      "forum9": {
+        "1": "mod/forum/view.php?id=4405543"
+      },
+      "forum10": {
+        "1": "mod/forum/view.php?id=4405544"
+      },
+      "forum11": {
+        "1": "mod/forum/view.php?id=4405547"
+      }
+    },
+    "module6": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4627098"
+      },
+      "forum1": {
+        "1": "mod/forum/view.php?id=4627109"
+      },
+      "forum2": {
+        "1": "mod/forum/view.php?id=4627113"
+      },
+      "forum3": {
+        "1": "mod/forum/view.php?id=4627117"
+      },
+      "forum4": {
+        "1": "mod/forum/view.php?id=4627130"
+      },
+      "forum5": {
+        "1": "mod/forum/view.php?id=4627133"
+      },
+      "forum6": {
+        "1": "mod/forum/view.php?id=4627134"
+      },
+      "forum7": {
+        "1": "mod/forum/view.php?id=4627138"
+      },
+      "forum8": {
+        "1": "mod/forum/view.php?id=4627142"
+      },
+      "forum9": {
+        "1": "mod/forum/view.php?id=4627136"
+      },
+      "forum10": {
+        "1": "mod/forum/view.php?id=4627160"
+      },
+      "forum11": {
+        "1": "mod/forum/view.php?id=4627170"
+      },
+      "forum12": {
+        "1": "mod/forum/view.php?id=4627174"
+      }
+    },
+    "module2": {
+      "comments": {
+        "1": "mod/forum/view.php?id=4405627"
+      },
+      "activities": {
+        "w1": {
+          "act3": {
+            "1": "mod/forum/view.php?id=4405609"
+          },
+          "act4": {
+            "1": "mod/forum/view.php?id=4405610"
+          }
+        },
+        "w2": {
+          "act1": {
+            "1": "mod/forum/view.php?id=4405611"
+          },
+          "act2": {
+            "1": "mod/forum/view.php?id=4405612"
+          },
+          "act3": {
+            "1": "mod/forum/view.php?id=4405613"
+          },
+          "act4": {
+            "1": "mod/forum/view.php?id=4405614"
+          }
+        },
+        "w3": {
+          "act2": {
+            "1": "mod/choice/view.php?id=4405608"
+          },
+          "act3": {
+            "1": "mod/forum/view.php?id=4405615"
+          },
+          "act4": {
+            "1": "mod/forum/view.php?id=4405616"
+          }
+        },
+        "w4": {
+          "assessed": {
+            "1": "mod/forum/view.php?id=4405677"
+          }
+        },
+        "w5": {
+          "assessed": {
+            "1": "mod/forum/view.php?id=4405693"
+          }
+        }
+      }
+    }
+  },
+  "ph": {
+    "module1": {
+      "general": {
+        "1": "mod/forum/view.php?id=2951321",
+        "2": "mod/forum/view.php?id=4913193"
+      },
+      "week1": {
+        "part3": {
+          "social": {
+            "act1": {
+              "1": "mod/choice/view.php?id=2951338",
+              "2": "mod/choice/view.php?id=4913218"
+            }
+          }
+        }
+      },
+      "week3": {
+        "activity": {
+          "1": "mod/forum/discuss.php?d=4625175",
+          "2": "mod/forum/discuss.php?d=4625175"
+        }
+      }
+    },
+    "module2": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=2932098",
+        "2": "mod/forum/view.php?id=4855811"
+      }
+    },
+    "module3": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=3096103",
+        "2": "mod/forum/view.php?id=4913340"
+      }
+    },
+    "module4": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=4625325"
+      },
+      "poll1": {
+        "1": "mod/choice/view.php?id=4625436"
+      },
+      "poll2": {
+        "1": "mod/choice/view.php?id=4625437"
+      },
+      "poll3": {
+        "1": "mod/choice/view.php?id=4625438"
+      },
+      "poll4": {
+        "1": "mod/choice/view.php?id=4625439"
+      },
+      "poll5": {
+        "1": "mod/choice/view.php?id=4625430"
+      },
+      "poll6": {
+        "1": "mod/choice/view.php?id=4625431"
+      },
+      "poll7": {
+        "1": "mod/choice/view.php?id=4625432"
+      },
+      "poll8": {
+        "1": "mod/choice/view.php?id=4625433"
+      },
+      "poll9": {
+        "1": "mod/choice/view.php?id=4625434"
+      },
+      "poll10": {
+        "1": "mod/choice/view.php?id=4625372"
+      }
+    },
+    "module5": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=2469235",
+        "2": "mod/forum/view.php?id=4814162"
+      },
+      "poll1": {
+        "1": "mod/choice/view.php?id=2469337",
+        "2": "mod/choice/view.php?id=4814228"
+      }
+    },
+    "module6": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=4913518"
+      },
+      "poll11": {
+        "1": "mod/choice/view.php?id=4913554"
+      }
+    },
+    "module7": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=4519901"
+      }
+    },
+     "module8": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=4520146"
+      },
+      "w1": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=2488717"
+        }
+      }
+    },
+    "module9": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=4625489"
+      }
+    },
+    "module10": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=4913677",
+        "2": "mod/forum/view.php?id=2942155"
+      }
+    },
+    "module11": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=2958006",
+        "2": "mod/forum/view.php?id=4913775"
+      }
+    },
+    "module12": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=4913839",
+        "2": "mod/forum/view.php?id=2960026"
+      }
+    },
+    "module13": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=2948091",
+        "2": "mod/forum/view.php?id=4913928"
+      }
+    }
+  },
+  "gfb": {
+    "module1": {
+      "w1": {
+        "act1": {
+          "1": "mod/choice/view.php?id=3231769"
+        },
+        "act2": {
+          "1": "mod/forum/view.php?id=3231534"
+        }
+      }
+    },
+    "module3": {
+      "discussion": {
+        "1": "mod/forum/view.php?id=3232107"
+      }
+    },
+    "module5": {
+      "quiz": {
+        "1": "mod/quiz/view.php?id=2406492"
+      },
+      "quiz2": {
+        "1": "mod/quiz/view.php?id=2513674"
+      }
+    },
+    "module6": {
+      "forum1": {
+        "1": "mod/forum/view.php?id=4549592"
+      },
+      "forum2": {
+        "1": "mod/forum/view.php?id=4549603"
+      },
+      "forum3": {
+        "1": "mod/forum/view.php?id=4549607"
+      }
+    },
+    "module7": {
+      "forum1": {
+        "1": "mod/forum/view.php?id=3218433"
+      }
+    },
+    "module8": {
+      "forum1": {
+        "1": "mod/forum/view.php?id=2772478"
+      }
+    },
+    "module11": {
+      "forum1": {
+        "1": "mod/forum/view.php?id=4780243"
+      }
+    }
+  },
+  "ia": {
+     "module1": {
+       "forum1": {
+          "1": "mod/forum/view.php?id=2472760",
+          "2": "mod/forum/view.php?id=4280214"
+       },
+       "forum2": {
+          "1": "mod/forum/view.php?id=2472766",
+          "2": "mod/forum/view.php?id=4280220"
+       },
+       "forum3": {
+          "1": "mod/forum/view.php?id=2472791",
+          "2": "mod/forum/view.php?id=4280246"
+       },
+       "forum4": {
+          "1": "mod/forum/view.php?id=2472819",
+          "2": "mod/forum/view.php?id=4280276"
+       },
+       "forum5": {
+          "1": "mod/forum/view.php?d=3558602"
+       }
+     },  
+ "module2": {
+       "forum1": {
+          "1": "mod/forum/view.php?id=4279656"
+       },
+       "forum2": {
+          "1": "mod/forum/view.php?id=4279662"
+       },
+       "forum3": {
+          "1": "mod/forum/view.php?id=4279689"
+       },
+       "forum4": {
+          "1": "mod/forum/view.php?id=4279723"
+       }
+     },
+     "module3": {
+       "forum1": {
+          "1": "mod/forum/view.php?id=4280562"
+        },
+       "forum2": {
+          "1": "mod/forum/view.php?id=4280570"
+        },
+       "forum3": {
+          "1": "mod/forum/view.php?id=4280597"
+        },
+       "forum4": {
+          "1": "mod/forum/view.php?id=4280619"
+        },
+       "forum5": {
+          "1": "mod/forum/view.php?id=4280645"
+        },
+       "forum6": {
+          "1": "mod/forum/view.php?id=4280670"
+        },
+       "forum7": {
+          "1": "mod/forum/view.php?id=4280694"
+        }
+      },
+      "module4": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=4920769"
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=4920773"
+        },
+        "forum3": {
+          "1": "mod/forum/view.php?id=4920776"
+        },
+        "forum4": {
+          "1": "mod/forum/view.php?id=4920777"
+        },
+        "forum5": {
+          "1": "mod/forum/view.php?id=4920779"
+        },
+        "forum6": {
+          "1": "mod/forum/view.php?id=4920780"
+        }
+      },
+      "module5": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=4278935"
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=4278958"
+         },
+        "forum3": {
+          "1": "mod/forum/view.php?id=4278962"
+        }
+      },    
+      "module6a": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=4858511"
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=4858552"
+        },
+        "forum5": {
+          "1": "mod/forum/view.php?id=4858594"
+        },
+        "forum3": {
+          "1": "mod/forum/view.php?id=4858659"
+        },
+        "forum4": {
+          "1": "mod/forum/view.php?id=4415789"
+        }
+       },
+      "module6b": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=4758307"
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=4758339"
+        },
+        "forum3": {
+          "1": "mod/forum/view.php?id=4758368"
+        }
+       },
+      "module7a": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=4924687"
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=4924722"
+        },
+        "forum3": {
+          "1": "mod/forum/view.php?id=4924741"
+        },
+        "forum4": {
+          "1": "mod/forum/view.php?id=4924758"
+        }
+       },
+      "module7b": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=4281452"
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=4281459"
+        },
+        "forum3": {
+          "1": "mod/forum/view.php?id=4281486"
+        },
+        "forum4": {
+          "1": "mod/forum/view.php?id=4281516"
+        }
+       },
+      "module7c": {
+        "forum3": {
+          "1": "mod/forum/view.php?id=2979036"
+        },
+        "forum5": {
+          "1": "mod/forum/view.php?id=2979078"
+        }
+       },
+      "module8a": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=4929833"
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=4929848"
+        },
+        "forum3": {
+          "1": "mod/forum/view.php?id=4929848"
+        },
+        "forum4": {
+          "1": "mod/forum/view.php?id=4929869"
+        }
+       },
+      "module8b": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=4936894"
+        },
+        "forum2": {
+          "1": "mod/forum/view.php?id=4936882"
+        },
+        "forum3": {
+          "1": "mod/forum/view.php?id=4936922"
+        }
+       },
+     "module10a": {
+        "forum1": {
+          "1": "mod/forum/view.php?id=4918028"
+        },
+       "forum2": {
+         "1": "mod/forum/view.php?id=4918054"
+       },
+       "forum3": {
+         "1": "mod/forum/view.php?id=4918078"
+       }
+     },
+    "module10b": {
+      "forum1": {
+        "1": "mod/forum/view.php?id=4920651"
+      },
+      "forum2": {
+        "1": "mod/forum/view.php?id=4920681"
+      },
+      "forum3": {
+        "1": "mod/forum/view.php?id=4920693"
+      }
+    }
+  }
+};
